104e0f671dd1e655d6f853f8fcfe985b3151a875
kwood <kwood@Keegan.>
2026-06-16 14:20:30 -0400

    Added in saves via Transactional

--- chatserver/src/main/java/com/chatap/chatserver/controller/ChatController.java
+++ chatserver/src/main/java/com/chatap/chatserver/controller/ChatController.java
@@ -1,6 +1,7 @@
 package com.chatapp.chatserver.controller;
 
 import com.chatapp.chatserver.model.ChatMessage;
+import com.chatapp.chatserver.service.MessageService;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
@@ -8,10 +9,21 @@ import java.time.Instant;
 
 @Controller
 public class ChatController{
+    private final MessageService messageService;
+    
+    public ChatController(MessageService messageService){
+        this.messageService = messageService;
+    }
+
     @MessageMapping("/chat")
     @SendTo("/topic/messages")
     public ChatMessage send(ChatMessage message){
-        return new ChatMessage(message.sender(), message.content(), Instant.now());
+        ChatMessage stamped = new ChatMessage(message.sender(), message.content(), Instant.now());
+
+        messageService.save(stamped);
+        return stamped;
     }
+
+
 
 }
--- chatserver/src/main/java/com/chatap/chatserver/service/MessageService.java
+++ chatserver/src/main/java/com/chatap/chatserver/service/MessageService.java
@@ -7,14 +7,20 @@ import tools.jackson.core.JacksonException;
 import java.time.Instant;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import com.chatapp.chatserver.model.MessageEntity;
+import com.chatapp.chatserver.repository.MessageRepository;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
 
 @Service
 public class MessageService{
     private static final Logger log = LoggerFactory.getLogger(MessageService.class);
     private final ObjectMapper objectMapper;
+    private final MessageRepository messageRepository;
 
-    public MessageService(ObjectMapper objectMapper){
+    public MessageService(ObjectMapper objectMapper,MessageRepository messageRepository){
         this.objectMapper = objectMapper;
+        this.messageRepository = messageRepository;
     }
 
 
@@ -43,6 +49,22 @@ public class MessageService{
 
     public ChatMessage systemMessage(String message){
         return new ChatMessage("SERVER", message, Instant.now());
+    }
+
+    public MessageEntity save(ChatMessage message){
+        MessageEntity m = new MessageEntity(message.sender(),message.content(), message.timestamp());
+        return messageRepository.save(m);
+    }
+
+
+    //Used for saving message data using Transactional to ensure clean reads
+    //Uses streams to take in all messages ordered by timesamp to map new chatmessages into the list
+    @Transactional(readOnly = true)
+    public List<ChatMessage> getRecentMessages(){
+        return messageRepository.findAllByOrderByTimestampDesc()
+                                .stream()
+                                .map(entity -> new ChatMessage(entity.getSender(),entity.getContent(),entity.getTimestamp()))
+                                .toList();
     }
         
 }
