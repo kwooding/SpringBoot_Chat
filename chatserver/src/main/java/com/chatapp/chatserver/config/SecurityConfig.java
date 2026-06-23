8079cd60ce384dc3b3182277e46ac106c5fa7083
kwood <kwood@Keegan.>
2026-06-23 15:15:02 -0400

    Actuator health checks added into Compose

--- chatserver/src/main/java/com/chatapp/chatserver/config/SecurityConfig.java
+++ chatserver/src/main/java/com/chatapp/chatserver/config/SecurityConfig.java
@@ -37,6 +37,7 @@ public class SecurityConfig{
             .authorizeHttpRequests(auth -> auth
                                     .requestMatchers("/api/auth/**").permitAll()
                                     .requestMatchers("/chat/**").permitAll()
+                                    .requestMatchers("/actuator/**").permitAll()
                                     .requestMatchers("/api/messages/**").authenticated()
                                     .anyRequest().authenticated());
 
